b4acf6784f8bc28700a9b1d48ff3f94101008181
GandaMitsuo <panpeikun@web.de>
2023-11-25 16:38:25 +0100

    fix path

--- src/app/portfolio/portfolio.component.ts
+++ src/app/portfolio/portfolio.component.ts
@@ -20,7 +20,7 @@ export class PortfolioComponent {
     {
       title: 'Join',
       types: 'Angular | TypeScript | HTML | CSS',
-      img: '/assets/img/join2.png',
+      img: './assets/img/join2.png',
       description: 'Task manager inspired by the Kanban System. Create and organize tasks using drag and drop functions, assign users and categories.',
       ref: 'https://bastian-riedmann.developerakademie.net/join/index.html',
       github: 'https://github.com/GandaMitsuo/Join',
@@ -28,7 +28,7 @@ export class PortfolioComponent {
     {
       title: 'Sharkie',
       types: 'JavaScript | HTML | CSS',
-      img: '/assets/img/sharki2.jpg',
+      img: './assets/img/sharki2.jpg',
       description: 'A simple Jump-and-Run game based on an object-oriented approach. Help shakrie to find coins and poison bottles to fight against the killer whale.',
       ref: 'https://bastian-riedmann.developerakademie.net/Sharki/index.html',
       github: 'https://github.com/GandaMitsuo/Sharki',
@@ -36,7 +36,7 @@ export class PortfolioComponent {
     {
       title: 'Pokédex',
       types: 'JavaScript | HTML | CSS | API',
-      img: '/assets/img/pokedex2.jpg',
+      img: './assets/img/pokedex2.jpg',
       description: 'Based on the PokéAPI a simple library that provides and catalogues pokemon information.',
       ref: 'https://bastian-riedmann.developerakademie.net/pokedex/index.html',
       github: 'https://github.com/GandaMitsuo/Pokedex',
@@ -44,7 +44,7 @@ export class PortfolioComponent {
     {
       title: 'Simple CRM - NOT YET READY',
       types: 'Angular | Firebase',
-      img: '/assets/img/crm.png',
+      img: './assets/img/crm.png',
       description: 'A very Simple Customer Relationship Management system working with CRUD functionality.',
       ref: '',
       github: '',
@@ -52,7 +52,7 @@ export class PortfolioComponent {
     {
       title: 'Portfolio',
       types: 'Angular | TypeScript | SCSS',
-      img: '/assets/img/portfolio.png',
+      img: './assets/img/portfolio.png',
       description: 'My portfolio showcases my front-end development projects, highlighting my coding skills, UI/UX sensibilities, and professional personality to demonstrate my expertise and experience in creating beautiful and functional web interfaces.',
       ref: '#header',
       github: 'https://github.com/GandaMitsuo/Portfolio.git',
@@ -60,7 +60,7 @@ export class PortfolioComponent {
     {
       title: 'Ring of Fire',
       types: 'Angular | TypeScript | Firebase | SCSS',
-      img: '/assets/img/ringoffire.jpg',
+      img: './assets/img/ringoffire.jpg',
       description: 'The Ring of Fire is a drinking game in which the players pull cards clockwise and have to perform specific actions depending on the card, while a glass with a schnapps mix is in the middle of the table.',
       ref: 'https://ring-of-fire-6bd03.web.app/',
       github: 'https://github.com/GandaMitsuo/RingOfFire',
